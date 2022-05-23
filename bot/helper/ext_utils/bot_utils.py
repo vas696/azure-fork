d675ee9cb155a4c10254324b2616137a691da0e8
Vasisth Roy <98415036+vas696@users.noreply.github.com>
2022-05-24 03:24:45 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -101,13 +101,13 @@ def getAllDownload(req_status: str):
     return None
 
 def get_progress_bar_string(status):
-    completed = status.processed_bytes() / 8
-    total = status.size_raw() / 8
+    completed = status.processed_bytes() / 10
+    total = status.size_raw() / 10
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
-    cFull = p // 8
-    p_str = '■' * cFull
-    p_str += '□' * (12 - cFull)
+    cFull = p // 10
+    p_str = '🔥' * cFull
+    p_str += '🕳️' * (10 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
